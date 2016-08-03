b52a98309626a0ed64cf456727024aa37c878738
komcal <kom.sujautra@gmail.com>
2016-08-03 14:14:42 +0700

    update sensor

--- final-exceed.ino
+++ final-exceed.ino
@@ -6,27 +6,36 @@
     PT_WAIT_WHILE(pt, millis()-ts < (ms));
 #define SMOKE A0
 #define SOUND A1
+#define TEMP A2
+#define GAS A5
 
-// struct pt pt_taskSmoke;
+struct pt pt_taskSmoke;
 struct pt pt_taskSound;
+struct pt pt_taskGas;
 struct pt pt_taskSerialEvent;
 struct pt pt_taskSendSerial;
+struct pt pt_taskTemp;
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 String dataToServer = "";
 String recieveData = "";
-int smokeData = 10;
+int smokeData = 0;
 int soundData = 0;
-String people = "low";
-
+int tempData = 0;
+String people = "";
+String name = "Bangkok%20saimai%201111";
+int humidData = 0;
+int height = 0;
+int gasData = 0;
+int temp;
 
 PT_THREAD(taskSound(struct pt* pt)) {
   static uint32_t ts;
   PT_BEGIN(pt);
   while (1) {
-    lcd.clear();
+    //lcd.clear();
     soundData = analogRead(SOUND);
-    lcd.print(soundData);
+    //lcd.print(soundData);
     if (soundData <= 200) {
       people = "Low";
     }
@@ -39,6 +48,46 @@ PT_THREAD(taskSound(struct pt* pt)) {
     else {
       people = "soundData error";
     }
+    PT_DELAY(pt, 500, ts);
+    PT_YIELD(pt);
+  }
+  PT_END(pt);
+}
+
+PT_THREAD(taskSmoke(struct pt* pt)) {
+  static uint32_t ts;
+  PT_BEGIN(pt);
+  while (1) {
+    //lcd.clear();
+    smokeData = analogRead(SMOKE);
+    //lcd.print(smokeData);
+    PT_DELAY(pt, 500, ts);
+    PT_YIELD(pt);
+  }
+  PT_END(pt);
+}
+
+PT_THREAD(taskGas(struct pt* pt)) {
+  static uint32_t ts;
+  PT_BEGIN(pt);
+  while (1) {
+    //lcd.clear();
+    gasData = analogRead(GAS);
+    //lcd.print(gasData);
+    PT_DELAY(pt, 500, ts);
+    PT_YIELD(pt);
+  }
+  PT_END(pt);
+}
+
+PT_THREAD(taskTemp(struct pt* pt)) {
+  static uint32_t ts;
+  PT_BEGIN(pt);
+  while (1) {
+    lcd.clear();
+    temp = analogRead(TEMP);
+    tempData = (temp*25-2050)/100;
+    lcd.print(tempData);
     PT_DELAY(pt, 500, ts);
     PT_YIELD(pt);
   }
@@ -68,8 +117,8 @@ PT_THREAD(taskSendSerial(struct pt* pt)){
   static uint32_t ts;
   PT_BEGIN(pt);
   while (1){
-    Serial1.println(recieveData+","+people+","+String(smokeData));
-    PT_DELAY(pt, 1000, ts);
+    Serial1.println(recieveData+","+name+","+tempData+","+people+","+humidData+","+height+","+String(smokeData)+","+gasData);
+    PT_DELAY(pt, 500, ts);
   }
   PT_END(pt);
 }
@@ -82,6 +131,9 @@ void setup() {
   PT_INIT(&pt_taskSerialEvent);
   PT_INIT(&pt_taskSendSerial);
   PT_INIT(&pt_taskSound);
+  PT_INIT(&pt_taskSmoke);
+  PT_INIT(&pt_taskGas);
+  PT_INIT(&pt_taskTemp);
 }
 
 void loop() {
@@ -89,4 +141,7 @@ void loop() {
   taskSerialEvent(&pt_taskSerialEvent);
   taskSendSerial(&pt_taskSendSerial);
   taskSound(&pt_taskSound);
+  taskSmoke(&pt_taskSmoke);
+  //taskGas(&pt_taskGas);
+  taskTemp(&pt_taskTemp);
 }
