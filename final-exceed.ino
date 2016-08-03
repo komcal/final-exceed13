d4e26ba1e84d55ad8e04432df481d5fbd687e811
komcal <kom.sujautra@gmail.com>
2016-08-03 13:13:48 +0700

    can read sound sensor

--- final-exceed.ino
+++ final-exceed.ino
@@ -8,7 +8,7 @@
 #define SOUND A1
 
 // struct pt pt_taskSmoke;
-// struct pt pt_taskSound;
+struct pt pt_taskSound;
 struct pt pt_taskSerialEvent;
 struct pt pt_taskSendSerial;
 
@@ -19,6 +19,32 @@ int smokeData = 10;
 int soundData = 0;
 String people = "low";
 
+
+PT_THREAD(taskSound(struct pt* pt)) {
+  static uint32_t ts;
+  PT_BEGIN(pt);
+  while (1) {
+    lcd.clear();
+    soundData = analogRead(SOUND);
+    lcd.print(soundData);
+    if (soundData <= 200) {
+      people = "Low";
+    }
+    else if (201 <= soundData && soundData <= 500) {
+      people = "Meduim";
+    }
+    else if (501 <= soundData) {
+        people = "High";
+    }
+    else {
+      people = "soundData error";
+    }
+    PT_DELAY(pt, 500, ts);
+    PT_YIELD(pt);
+  }
+  PT_END(pt);
+}
+
 PT_THREAD(taskSerialEvent(struct pt* pt)){
   static uint32_t ts;
   PT_BEGIN(pt);
@@ -28,6 +54,7 @@ PT_THREAD(taskSerialEvent(struct pt* pt)){
       str.replace("\r","");
       str.replace("\n","");
       Serial.println(str);
+      if(str != "") recieveData = str;
     }
     if(Serial.available()) {
       recieveData = Serial.readStringUntil('\r');
@@ -42,7 +69,7 @@ PT_THREAD(taskSendSerial(struct pt* pt)){
   PT_BEGIN(pt);
   while (1){
     Serial1.println(recieveData+","+people+","+String(smokeData));
-    PT_DELAY(pt, 500, ts);
+    PT_DELAY(pt, 1000, ts);
   }
   PT_END(pt);
 }
@@ -54,18 +81,12 @@ void setup() {
 
   PT_INIT(&pt_taskSerialEvent);
   PT_INIT(&pt_taskSendSerial);
+  PT_INIT(&pt_taskSound);
 }
 
 void loop() {
+  lcd.setCursor(0, 0);
   taskSerialEvent(&pt_taskSerialEvent);
   taskSendSerial(&pt_taskSendSerial);
-}
-
-void SerialEvent() {
-  if (Serial1.available()) {
-      String str = Serial1.readStringUntil('\r');
-      str.replace("\r","");
-      str.replace("\n","");
-      Serial.println(str);
-  }
+  taskSound(&pt_taskSound);
 }
